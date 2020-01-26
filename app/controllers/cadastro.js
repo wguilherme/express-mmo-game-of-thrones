473ac2dba07ca60bdd2ae78838e61c9a281b79ee
W. Guilherme <wguilhermecriativo@gmail.com>
2020-01-26 20:59:57 -0300

    [Fix] Express-validator version 5.3.1

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -3,6 +3,27 @@ module.exports.cadastro = (application, req, res) => {
 }
 
 module.exports.cadastrar = (application, req, res) => {
-res.send('teste - vamos cadastrar')
+
+    var dadosForm = req.body;
+
+    //validacoes
+    req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+    req.assert('usuario', 'Usuário pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha pode ser vazio').notEmpty();
+    req.assert('casa', 'Casa pode ser vazio').notEmpty();
+
+    //recuperar erros
+    const erros = req.validationErrors();
+
+    
+
+    if(erros){
+        res.send('existem erros no formulário');
+        return;
+    }
+
+    res.send('podemos cadastrar')
+
+
 
 }
